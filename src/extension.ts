819a8b76c81242a0d05118b189513d79ca3f6190
达峰的夏天 <xudafeng@126.com>
2023-09-17 23:37:14 +0800

    feat: add sidebar view (#5)
    * feat: add sidebar view
    
    * feat: add completion

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,28 @@
 import * as vscode from 'vscode';
 import { getWebviewContent } from './assets-maganer';
+import { completions } from './proxy/openai';
+
+class MyDataProvider {
+  getTreeItem(element: any) {
+    return element;
+  }
+
+  getChildren(element: any) {
+    if (!element) {
+      return [
+        new vscode.TreeItem('Item 1'),
+        new vscode.TreeItem('Item 2')
+      ];
+    }
+    return [];
+  }
+}
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('vscode-extension-bootstrap is active.');
 
   const webViewPanel = vscode.window.createWebviewPanel(
-    'myCustomWebview',
+    'myCustomView',
     '自定义Webview',
     vscode.ViewColumn.Beside,
     {
@@ -19,11 +36,20 @@ export function activate(context: vscode.ExtensionContext) {
   webViewPanel.webview.html = getWebviewContent();
   context.subscriptions.push(webViewPanel);
 
+  const dataProvider = new MyDataProvider();
+  vscode.window.registerTreeDataProvider('myCustomView', dataProvider);
+
   let disposable = vscode.commands.registerCommand('vscode-extension-bootstrap.helloWorld', () => {
     console.log('hello');
   });
 
   context.subscriptions.push(disposable);
+
+  completions().then(d => {
+    console.log(d);
+  }).catch(e => {
+    console.log(e);
+  });
 };
 
 export function deactivate() {
